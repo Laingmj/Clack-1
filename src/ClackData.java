6554944955409f3bd918d32db25f6793366fb86b
laingmj <laingmj@clarkson.edu>
2022-10-03 23:04:18 -0400

    added ClackData removed Test

---
+++ src/ClackData.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+
+public abstract class ClackData {
+    private String username;
+    private int type;
+    private Date date;
+
+    public ClackData(String username, int type) {
+        this.username = username;
+        this.type = type;
+    }
+
+    public ClackData(int type){
+        this("Anon", type);
+    }
+
+    public ClackData(){
+        this("Anon", 1);
+    }
+
+    public int getType(){
+        return type;
+    }
+    public String getUsername(){
+        return username;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public abstract String getData();
+}
